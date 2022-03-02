ecf63df7083be6674c6682265e50f979289879d6
unknown <arubagas16@gmail.com>
2022-03-02 19:53:25 +0700

    add validation

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ const addButton = document.getElementById('addButton');
 const result = document.getElementById('result');
 
 addButton.addEventListener('click', () => {
+    const inputOnevalue = inputOne.value;
+    const inputTwovalue = inputTwo.value;
+    if(validation(inputOnevalue, inputTwovalue) === true){
     result.innerHTML = parseInt(inputOne.value) + parseInt(inputTwo.value);
+    }
 });
+
+const validation = (inputOne, inputTwo) =>{
+    if((numberOne == false)  || (numberTwo == false) || (isNaN(numberOne)) || (isNaN(numberTwo))){
+        return false;
+    }
+    return true;
+}
